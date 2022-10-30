61924a13ab65beabceab7859a159a6f6f37cf336
JairoSotomayorG <jairof117@hotmail.com>
2022-10-30 09:13:13 -0500

    interpolacion con funciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyect000-curso';
+  nombre = 'Jairo Sotomayor';
+  edad = 37;
+  email = 'rpablo@gmail.com';
+  sueldos = [1700, 1600, 1900];
+  activo = true;
+
+  esActivo(){
+    if (this.activo)
+        return 'trabajador Activo';
+      else
+        return 'trabajador inactivo';
+  }
+
+  ultimos3Sueldos(){
+    let suma = 0
+    for(let x =0; x<this.sueldos.length; x++)
+    suma+=this.sueldos[x];
+    return suma;
+  }
 }
